98a11f9127bc6a2af012c6b845b958643f624cd4
yubeom <kimub1204@mz.co.kr>
2024-07-03 10:11:26 +0900

    init: Vue@2.7 Project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, reactive } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+const count = ref(0);
+const state = reactive({
+  count,
+});
+
+const handleUp = () => {
+  count.value++;
+};
+
+const handleDown = () => {
+  count.value--;
+};
+</script>
+
+<template>
+  <div id="app">
+    <hello-world :count="count" />
+    <button type="button" @click="handleUp">버튼</button>
+    <button type="button" @click="handleDown">버튼</button>
+  </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed, reactive } from "vue";
+
+const props = defineProps(["count"]);
+
+// console.log(props.count);
+
+const state = reactive({
+  message: computed(() => {
+    if (props.count == 0) {
+      return "0";
+    }
+
+    return props.count > 0 ? "양수" : "음수";
+  }),
+});
+</script>
+
+<template>
+  <div>
+    <h1>카운트: {{ props.count }}</h1>
+    <p>{{ state.message }}</p>
+  </div>
+</template>
